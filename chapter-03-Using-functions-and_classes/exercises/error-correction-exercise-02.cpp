223650c03e0e29f4599dd02467445ab2cbe7dcc4
Anu Antony <antonyanu275@gmail.com>
2023-11-02 15:10:06 +0530

    error correction problem addded

---
+++ chapter-03-Using-functions-and_classes/exercises/error-correction-exercise-02.cpp
@@ -0,0 +1,53 @@
+/*
+
+The program on the opposite page contains several errors! Correct the errors
+and ensure that the program can be executed
+
+// A program containing errors!
+# include <iostream>, <string>
+# include <stdlib>
+# void srand( seed);
+int main()
+{
+string message "\nLearn from your mistakes!";
+cout << message << endl;
+int len = length( message);
+cout << "Length of the string: " << len << endl;
+// And a random number in addition:
+int a, b;
+a = srand(12.5);
+b = rand( a );
+cout << "\nRandom number: " << b << endl;
+return 0;
+}
+
+*/
+
+
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+
+string message("\nLearn from your mistakes!");
+
+cout << message << endl;
+
+int len = message.length();
+
+cout << "Length of the string : " << len << endl;
+
+// And a random number in addition:
+ int b;
+srand(12);
+b = rand();
+cout << "\nRandom number: " << b << endl;
+
+return 0;
+
+}
